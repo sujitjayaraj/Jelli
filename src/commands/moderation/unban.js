2da06df1934136d2a3fea1044aed753724e3e53f
Sujit Jayaraj <sujitjayaraj@live.com>
2021-01-28 16:56:06 -0500

    Added the ban and unban commands

---
+++ src/commands/moderation/unban.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: "unban",
+    description: "Unbans the `user` with the provided `reason`",
+    usage: "-unban @user reason",
+
+    async execute(app, msg, args){
+        if(!msg.member.hasPermission("BAN_MEMBERS")) return;
+        if(!msg.guild.me.hasPermission("BAN_MEMBERS")){
+            msg.react(app.config.reactions.cross);
+            msg.channel.send("❌ I do not have the `BAN_MEMBERS` permission, please check my `BAN_MEMBERS` role permission and try again!");
+            return;
+        }
+        const guildMembers = await app.parser.getIds(msg, args);
+        const offenderID = guildMembers.length != 0?guildMembers[0]:undefined
+        if(offenderID !== undefined){
+            let reason = args.slice(1).join(" ");
+            reason = reason.length == 0? "No reason provided" : reason;
+            try{
+                offender = await msg.guild.members.unban(offenderID, reason);
+                msg.react(app.config.reactions.tick);
+                msg.channel.send({embed:{
+                    description: `**${offender.tag} has been unbanned** | ${reason}`,
+                    color: 0x00ff00
+                }});
+                app.db.modlogs.insertOne({"offender_id":offender.id, "offender_name":offender.tag, "action":"unban", "reason":reason, "timestamp":msg.createdTimestamp, "moderator_id":msg.author.id, "moderator_name":msg.author.tag, "guild_id":msg.guild.id});
+            }
+            catch(error){
+                msg.react(app.config.reactions.cross);
+                msg.channel.send(`❌ Unable to unban the provided user. Please enter a valid user ID or mention!`);
+            }
+        }
+        else{
+            msg.react(app.config.reactions.cross);
+            msg.channel.send(`❌ No user ID has been provided!`);
+        }
+    }
+}
--- src/utility/Parser.js
+++ src/utility/Parser.js
@@ -15,5 +15,12 @@ class Parser{
         })
         return guildMembers;
     }
+    async getIds(msg, args){
+        let guildMembers = [];
+        args.map(async (arg, i) => {
+            if(/^<?@?!?(\d+)>?$/.test(arg)) guildMembers.push(arg.match(/^<?@?!?(\d+)>?$/)[1]);
+        });
+        return guildMembers;
+    }
 }
 module.exports = Parser;
